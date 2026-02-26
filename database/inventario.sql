03b482f119387bf563b00856a1ccb48ca1f88887
alanpiguaveruiz <a.piguave@eston.ecotec.edu.ec>
2026-02-26 17:47:49 -0300

    feat(db): crear tablas ventas y detalle_ventas con claves foráneas
    - Se crea tabla ventas con campos:
      id, fecha, total, estado
    
    - Se crea tabla detalle_ventas con relación a:
      ventas (venta_id)
      productos (producto_id)
    
    - Se agregan claves foráneas con ON DELETE CASCADE
    
    Esto permite implementar ventas múltiples con relación 1:N
    y mantener integridad referencial en el sistema de inventario.

--- database/inventario.sql
+++ database/inventario.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 26-02-2026 a las 01:54:49
+-- Tiempo de generación: 26-02-2026 a las 21:44:38
 -- Versión del servidor: 8.4.8
 -- Versión de PHP: 8.2.12
 
@@ -20,6 +20,31 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `inventario`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalle_ventas`
+--
+
+CREATE TABLE `detalle_ventas` (
+  `id` int NOT NULL,
+  `venta_id` int NOT NULL,
+  `producto_id` int NOT NULL,
+  `cantidad` int NOT NULL,
+  `precio_unitario` decimal(10,2) NOT NULL,
+  `subtotal` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `detalle_ventas`
+--
+
+INSERT INTO `detalle_ventas` (`id`, `venta_id`, `producto_id`, `cantidad`, `precio_unitario`, `subtotal`) VALUES
+(1, 1, 4, 1, 10.00, 10.00),
+(2, 1, 3, 2, 12.90, 25.80),
+(3, 1, 1, 3, 25.50, 76.50),
+(4, 2, 4, 55, 10.00, 550.00);
 
 -- --------------------------------------------------------
 
@@ -43,13 +68,42 @@ CREATE TABLE `productos` (
 --
 
 INSERT INTO `productos` (`id`, `nombre`, `tipo`, `descripcion`, `precio`, `stock`, `unidad`, `created_at`) VALUES
-(1, 'Fertilizante NPK 15-15-15', 'Fertilizante', 'Estimula el crecimiento del cultivo', 25.50, 40, 'Saco 25kg', '2026-02-26 00:42:32'),
-(2, 'Abono Orgánico Compost', 'Abono', 'Mejora la fertilidad del suelo', 10.00, 60, 'Bolsa 10kg', '2026-02-26 00:42:32'),
-(3, 'Insecticida Natural Neem', 'Insecticida', 'Control de plagas sin químicos', 12.90, 35, 'Litro', '2026-02-26 00:42:32');
+(1, 'Fertilizante NPK 15-15-15', 'Fertilizante', 'Estimula el crecimiento del cultivo', 25.50, 37, 'Saco 25kg', '2026-02-26 00:42:32'),
+(3, 'Insecticida Natural Neem', 'Insecticida', 'Control de plagas sin químicos', 12.90, 33, 'Litro', '2026-02-26 00:42:32'),
+(4, 'Semillon', 'Abono', 'producto que hace crecer rapido las semillas de girasol', 10.00, 41, '2kg', '2026-02-26 16:21:59');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ventas`
+--
+
+CREATE TABLE `ventas` (
+  `id` int NOT NULL,
+  `fecha` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `total` decimal(10,2) NOT NULL,
+  `estado` enum('Pendiente','Pagada','Anulada') COLLATE utf8mb4_general_ci DEFAULT 'Pendiente'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `ventas`
+--
+
+INSERT INTO `ventas` (`id`, `fecha`, `total`, `estado`) VALUES
+(1, '2026-02-26 16:50:42', 112.30, 'Pagada'),
+(2, '2026-02-26 16:51:38', 550.00, 'Pagada');
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `detalle_ventas`
+--
+ALTER TABLE `detalle_ventas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `venta_id` (`venta_id`),
+  ADD KEY `producto_id` (`producto_id`);
 
 --
 -- Indices de la tabla `productos`
@@ -58,14 +112,43 @@ ALTER TABLE `productos`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `ventas`
+--
+ALTER TABLE `ventas`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
+
+--
+-- AUTO_INCREMENT de la tabla `detalle_ventas`
+--
+ALTER TABLE `detalle_ventas`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
   MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `ventas`
+--
+ALTER TABLE `ventas`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `detalle_ventas`
+--
+ALTER TABLE `detalle_ventas`
+  ADD CONSTRAINT `fk_producto` FOREIGN KEY (`producto_id`) REFERENCES `productos` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `fk_venta` FOREIGN KEY (`venta_id`) REFERENCES `ventas` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
