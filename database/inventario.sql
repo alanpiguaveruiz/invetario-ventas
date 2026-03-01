423708954cc87746567da9d51697b074c8c02aea
alanpiguaveruiz <a.piguave@eston.ecotec.edu.ec>
2026-02-28 23:58:22 -0300

    Actualización de la base de datos

--- database/inventario.sql
+++ database/inventario.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 26-02-2026 a las 21:44:38
+-- Tiempo de generación: 01-03-2026 a las 03:56:27
 -- Versión del servidor: 8.4.8
 -- Versión de PHP: 8.2.12
 
@@ -41,10 +41,15 @@ CREATE TABLE `detalle_ventas` (
 --
 
 INSERT INTO `detalle_ventas` (`id`, `venta_id`, `producto_id`, `cantidad`, `precio_unitario`, `subtotal`) VALUES
-(1, 1, 4, 1, 10.00, 10.00),
-(2, 1, 3, 2, 12.90, 25.80),
 (3, 1, 1, 3, 25.50, 76.50),
-(4, 2, 4, 55, 10.00, 550.00);
+(5, 3, 1, 12, 25.50, 306.00),
+(7, 4, 1, 4, 25.50, 102.00),
+(8, 5, 1, 4, 25.50, 102.00),
+(9, 6, 1, 1, 25.50, 25.50),
+(10, 7, 1, 1, 25.50, 25.50),
+(11, 8, 1, 4, 25.50, 102.00),
+(13, 9, 1, 2, 25.50, 51.00),
+(14, 10, 1, 7, 25.50, 178.50);
 
 -- --------------------------------------------------------
 
@@ -68,9 +73,9 @@ CREATE TABLE `productos` (
 --
 
 INSERT INTO `productos` (`id`, `nombre`, `tipo`, `descripcion`, `precio`, `stock`, `unidad`, `created_at`) VALUES
-(1, 'Fertilizante NPK 15-15-15', 'Fertilizante', 'Estimula el crecimiento del cultivo', 25.50, 37, 'Saco 25kg', '2026-02-26 00:42:32'),
-(3, 'Insecticida Natural Neem', 'Insecticida', 'Control de plagas sin químicos', 12.90, 33, 'Litro', '2026-02-26 00:42:32'),
-(4, 'Semillon', 'Abono', 'producto que hace crecer rapido las semillas de girasol', 10.00, 41, '2kg', '2026-02-26 16:21:59');
+(1, 'Fertilizante NPK 15-15-15', 'Fertilizante', 'Estimula el crecimiento del cultivo', 25.50, 32, 'Saco 25kg', '2026-02-26 00:42:32'),
+(2, 'Insecticida Natural Neem', 'Insecticida', 'Control de plagas sin químicos', 12.90, 33, 'Litro', '2026-02-26 03:42:32'),
+(3, 'Semillon', 'Abono', 'producto que hace crecer rapido las semillas de girasol', 10.00, 41, '2kg', '2026-02-26 19:21:59');
 
 -- --------------------------------------------------------
 
@@ -90,8 +95,16 @@ CREATE TABLE `ventas` (
 --
 
 INSERT INTO `ventas` (`id`, `fecha`, `total`, `estado`) VALUES
-(1, '2026-02-26 16:50:42', 112.30, 'Pagada'),
-(2, '2026-02-26 16:51:38', 550.00, 'Pagada');
+(1, '2026-02-26 16:50:42', 112.30, 'Anulada'),
+(2, '2026-02-26 16:51:38', 550.00, 'Anulada'),
+(3, '2026-02-26 20:55:58', 486.60, 'Anulada'),
+(4, '2026-02-26 22:46:02', 102.00, 'Anulada'),
+(5, '2026-02-26 22:51:49', 102.00, 'Anulada'),
+(6, '2026-02-27 11:30:58', 25.50, 'Anulada'),
+(7, '2026-02-27 11:32:57', 25.50, 'Anulada'),
+(8, '2026-02-27 12:16:23', 122.00, 'Anulada'),
+(9, '2026-02-27 20:56:35', 51.00, 'Anulada'),
+(10, '2026-02-28 19:00:54', 178.50, 'Pagada');
 
 --
 -- Índices para tablas volcadas
@@ -125,7 +138,7 @@ ALTER TABLE `ventas`
 -- AUTO_INCREMENT de la tabla `detalle_ventas`
 --
 ALTER TABLE `detalle_ventas`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT de la tabla `productos`
@@ -137,7 +150,7 @@ ALTER TABLE `productos`
 -- AUTO_INCREMENT de la tabla `ventas`
 --
 ALTER TABLE `ventas`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Restricciones para tablas volcadas
